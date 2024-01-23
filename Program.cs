4e31efe1a1e8c1bd84a0f598fba3fb2ff43a8b9c
skaylean4 <skaylean444@gmail.com>
2024-01-23 11:44:09 +0200

    totalcommit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Colsole.WriteLine(Hello World);  
+
+
+using System.Runtime.CompilerServices;
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
